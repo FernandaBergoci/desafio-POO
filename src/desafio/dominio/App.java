1949f2510c9512848df4c3d59e7082b68461ac37
Fernanda Bergoci <fernandabergoci@gmail.com>
2022-07-28 20:55:09 -0300

    abstracao e encapsulamento

---
+++ src/desafio/dominio/App.java
@@ -0,0 +1,26 @@
+package desafio.dominio;
+
+import java.time.LocalDate;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(8);
+        
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso js");
+        curso2.setDescricao("Descrição curso js");
+        curso2.setCargaHoraria(4);
+        
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setDate(LocalDate.now());
+        
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
---
+++ src/desafio/dominio/Mentoria.java
@@ -0,0 +1,40 @@
+package desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descricao;
+    private LocalDate date;
+
+    
+    public Mentoria() {
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    @Override
+    public String toString() {
+        return "Mentoria [date=" + date + ", descricao=" + descricao + ", titulo=" + titulo + "]";
+    }
+
+    
+
+    
+}
