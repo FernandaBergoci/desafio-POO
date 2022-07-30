c1b722f4299db89f0e83a4bb5a4d66871d8a3c8a
Fernanda Bergoci <fernandabergoci@gmail.com>
2022-07-30 17:24:21 -0300

    Versão final

--- src/desafio/dominio/Mentoria.java
+++ src/desafio/dominio/Mentoria.java
@@ -2,27 +2,22 @@ package desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo {
+    
     private LocalDate date;
 
     
     public Mentoria() {
     }
+
     
-    public String getTitulo() {
-        return titulo;
+    @Override
+    public double calcularXp() {
+    
+        return XP_PADRAO + 20d;
     }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+
+
     public LocalDate getDate() {
         return date;
     }
@@ -31,7 +26,7 @@ public class Mentoria {
     }
     @Override
     public String toString() {
-        return "Mentoria [date=" + date + ", descricao=" + descricao + ", titulo=" + titulo + "]";
+        return "Mentoria [date=" + date + ", descricao=" + getDescricao() + ", titulo=" + getTitulo() + "]";
     }
 
     
